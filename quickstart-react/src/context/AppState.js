16619bf3875a14131151ab6f873591a4f47ee148
Omer Nave <>
2022-05-01 12:33:45 +0300

    export context

--- quickstart-react/src/context/AppState.js
+++ quickstart-react/src/context/AppState.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { createContext } from "react";
 
+export const AppContext = createContext();
 const AppState = ({ children }) => {
-  const Context = createContext();
   const [theme, setTheme] = useState("light");
   const [boardData, setBoardDate] = useState(undefined);
   const value = { theme, setTheme, boardData, setBoardDate };
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export default AppState;
